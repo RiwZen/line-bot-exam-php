5edde9d7dc1e2f8cb650e3bfdcc14d81651c3745
RiwZen <41966618+RiwZen@users.noreply.github.com>
2018-08-01 00:02:54 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = 'a/NPvKHsnHqQyHxDX+nSpmJai07Ofr766akGt9fG3+2tVaRoyOvY9AHtHsOqnCKCS5vdBSg6ROCpMkffWAb/NzazBWhuEXoi4TgAKy/3fDGEsG1gqTRTI1JyVYLyiq9GoEehYhUu/ckoqGQdQMeTuAdB04t89/1O/w1cDnyilFU=
+$access_token = 'JhRIukA7CpaetJPRs31gQ/wFm+FWngaiGHA6B8YtwhMG5mHejk3nWLKrcLT796MLS5vdBSg6ROCpMkffWAb/NzazBWhuEXoi4TgAKy/3fDGq7ulJdPW5xNCaF6y5ZT+b1nxVJ70Q9ozN/ZaNSGabCQdB04t89/1O/w1cDnyilFU=
 ';
 
 // Get POST body content
