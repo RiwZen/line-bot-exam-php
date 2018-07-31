d76fda4ea6b4af91a7272142c03d0e8d05c47e5b
RiwZen <41966618+RiwZen@users.noreply.github.com>
2018-08-01 00:08:56 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,11 +4,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'JhRIukA7CpaetJPRs31gQ/wFm+FWngaiGHA6B8YtwhMG5mHejk3nWLKrcLT796MLS5vdBSg6ROCpMkffWAb/NzazBWhuEXoi4TgAKy/3fDGq7ulJdPW5xNCaF6y5ZT+b1nxVJ70Q9ozN/ZaNSGabCQdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '68c8ac7c490086db5cc65135ced4908b';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'Uacc735178303bcaeef074a2ea00c5840';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
